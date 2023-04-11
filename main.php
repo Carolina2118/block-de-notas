9368676e8fd6a4708d8d5bd1226bc97e48a4c002
Carolina2118 <loboscarolinaa@gmail.com>
2023-04-11 19:48:30 -0400

    retocando

--- main.php
+++ main.php
@@ -1,8 +1,20 @@
 <?php
-  print_r($_REQUEST);
+$titulo = $_REQUEST["titulo"];
+$nota  = $_REQUEST["nota"];
 
-echo "
+echo " 
+ <form action ='index.php '>
+ <div style = 'background-color: #8EC5FC;
+ background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);margin-left:116px ; width:369px;
+ '>
+ <p name='nota'>$titulo </p>
+ <p name='nota'style= 'background-color: #8EC5FC;
+ background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);
+ '>$nota</p>
+ </div> 
+ </form>
+ "
+# print_r($_REQUEST);
 
 
-"
 ?>
