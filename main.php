ed61c3afcefadf1b4fa83543fe2dc083927049ab
Carolina2118 <loboscarolinaa@gmail.com>
2023-04-20 00:30:26 -0400

    nodificando ul / li /m

--- main.php
+++ main.php
@@ -3,18 +3,23 @@ $titulo = $_REQUEST["titulo"];
 $nota  = $_REQUEST["nota"];
 
 echo " 
- <form action ='index.php '>
- <div style = 'background-color: #8EC5FC;
- background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);margin-left:116px ; width:369px;
- '>
- <p name='nota'>$titulo </p>
- <p name='nota'style= 'background-color: #8EC5FC;
- background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);
- '>$nota</p>
- </div> 
+ <form action ='inicio.php'>
+     <div style='border: 2px solid; width: 300px; background-color: #0093E9;
+    background-image: linear-gradient(160deg, #0093E9 0%, #80D0C7 24%, #f5eaea 49%, #68bf68 75%, #406d3a 100%);
+    
+    '>
+        <div style=''> 
+            
+            <p name='nota' style=''>$titulo </p>
+        </div>
+        <div stytle=''>
+            <p name='notas' style =''>$nota</p>
+        </div>
+    </div>
  </form>
  "
-# print_r($_REQUEST);
-
+ # print_r($_REQUEST);
+ # background-color: #8EC5FC;background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);margin-left:116px ; width:369px;'>
+ #background-color: rgba(255, 255, 255, 0.8);
 
 ?>
